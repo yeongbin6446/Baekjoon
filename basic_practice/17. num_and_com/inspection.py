5660af854e3206b877317a9c074e70b3bbb245c1
Yeongbin <yeongbin6446@gmail.com>
2022-03-20 17:14:16 +0900

    실 패

---
+++ basic_practice/17. num_and_com/inspection.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+nums = []
+for i in range(int(input())):
+    n = int(sys.stdin.readline())
+    nums.append(n)
+
+if len(nums) == 2:
+    g = abs(nums[1]-nums[0])
+else:
+    for i in range(2, len(nums)):
+        g = abs(nums[1] - nums[0])
+        g = math.gcd(g, abs(nums[i] - nums[i - 1]))
+
+ans = []
+lg = int((g**0.5))
+for i in range(2,lg+1):
+    if g % i == 0:
+        ans.append(i)
+        ans.append(g//i)
+
+ans.append(g)
+ans = list(set(ans))
+ans.sort()
+print(*ans)
