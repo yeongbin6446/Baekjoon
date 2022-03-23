321cd33cf3cea97f096711238666507c426aec9e
Yeongbin <yeongbin6446@gmail.com>
2022-03-23 16:55:37 +0900

    add : dp

--- basic_practice/15. dp/fibo.py
+++ basic_practice/15. dp/fibo.py
@@ -1,10 +1,13 @@
+
+
 def fibo(n: int) -> int:
+    memo = [[0 for col in range(2)] for row in range(n + 1)]
+
     if n == 0:
         return [1, 0]
     elif n == 1:
         return [0, 1]
     else:
-        memo = [[0 for col in range(2)] for row in range(n + 1)]
         memo[0] = [1, 0]
         memo[1] = [0, 1]
         for i in range(2, n + 1):
@@ -17,3 +20,4 @@ N = int(input())
 for i in range(N):
     n = int(input())
     print(*fibo(n))
+
--- basic_practice/15. dp/func.py
+++ basic_practice/15. dp/func.py
@@ -1,27 +1,25 @@
-import sys
+from sys import stdin
 
-reg = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+memo = [[[0 for i in range(21)] for j in range(21)] for k in range(21)]
 
 def w(a, b, c):
-    global reg
-
+    global memo
     if a <= 0 or b <= 0 or c <= 0:
         return 1
     if a > 20 or b > 20 or c > 20:
-        return w(20, 20, 20)
-    if reg[a][b][c] != 0:
-        return reg[a][b][c]
+        return w(20,20,20)
+    if memo[a][b][c] != 0:
+        return memo[a][b][c]
     if a < b < c:
-        reg[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
-        return reg[a][b][c]
-
-    reg[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
-    return reg[a][b][c]
-
+        memo[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return memo[a][b][c]
+    else:
+        memo[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return memo[a][b][c]
 
 while True:
-    a, b, c = map(int, sys.stdin.readline().split())
-    if a == b == c == -1:
+    a, b, c = map(int, stdin.readline().split())
+    if a == -1 and b == -1 and c == -1:
         break
 
-    print("w(%d, %d, %d) = %d" % (a, b, c, w(a, b, c)))
+    print('w({}, {}, {}) = {}'.format(a, b, c, w(a,b,c)))
